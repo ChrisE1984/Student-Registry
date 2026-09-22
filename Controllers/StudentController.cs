623f0924fbc5e6c4712fb63dfbf1a0c3a53763f1
ChrisE1984 <chris.estrada1984@proton.me>
2026-09-22 14:06:51 -0700

    additional update with email find

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -9,10 +9,14 @@ namespace Student_Registry.Controllers
     public class StudentController : ControllerBase
     {
         static List<StudentRegistry> Students = [
-            new StudentRegistry {Id = 1, Firstname = "Andre", Lastname = "Noszick", Hobby = "Flute", Email = "twilightbreakingdawndres@gmail.com", Slackname = "Andre3000"},
-            new StudentRegistry {Id = 1, Firstname = "Rodney", Lastname = "Ruxin", Hobby = "Fantasy Football", Email = "shivachamp@gmail.com", Slackname = "TheChamp"},
-            new StudentRegistry {Id = 1, Firstname = "Taco", Lastname = "MacAurther", Hobby = "Taco", Email = "passwordistaco@gmail.com", Slackname = "MrMcGiblets"}
+            new StudentRegistry {Id = 1, Firstname = "Callen", Lastname = "Thomason", Hobby = "Gettin' Swole", Email = "cthomason@codestack.co", Slackname = "Callen Tomason"},
+            new StudentRegistry {Id = 2, Firstname = "Valery", Lastname = "Lot", Hobby = "Trying new restaurants", Email = "vlot@codestack.co", Slackname = "Valery Lot"},
+            new StudentRegistry {Id = 3, Firstname = "Zionn", Lastname = "Showers", Hobby = "Gaming", Email = "zshowers@codestack.co", Slackname = "Zionn Showers"},
+            new StudentRegistry {Id = 4, Firstname = "Chris", Lastname = "Estrada", Hobby = "Magic: The Gathering", Email = "cestrada@codestack.co", Slackname = "Chris Estrada"},
+            new StudentRegistry {Id = 5, Firstname = "Brandon", Lastname = "Langehennig", Hobby = "Art", Email = "blangehennig@codestack.co", Slackname = "Brandon Langehennig"},
+            new StudentRegistry {Id = 6, Firstname = "Zackary", Lastname = "Santos", Hobby = "Gaming", Email = "zsantoss@codestack.co", Slackname = "Zackary"}
         ];
+
 
         private static int _nextID = 4;
 
@@ -24,7 +28,7 @@ namespace Student_Registry.Controllers
         }
 
         [HttpGet("GetStudent/{id}")]
-        public ActionResult<StudentRegistry> GetByID (int id)
+        public ActionResult<StudentRegistry> GetByID(int id)
         {
             StudentRegistry? student = Students.FirstOrDefault(c => c.Id == id);
 
@@ -36,17 +40,31 @@ namespace Student_Registry.Controllers
             return Ok(student);
         }
 
+        [HttpGet("GetStudentEmail/{email}")]
+        public ActionResult<StudentRegistry> GetByEmail(string email)
+        {
+
+            StudentRegistry? student = Students.FirstOrDefault(c => c.Email == email);
+
+            if (student == null)
+            {
+                return NotFound($"No student found with email {email}.");
+            }
+
+            return Ok(student);
+        }
+
         [HttpPost("Add")]
-        public ActionResult<StudentRegistry> Create ([FromBody] StudentRegistry incoming)
+        public ActionResult<StudentRegistry> Create([FromBody] StudentRegistry incoming)
         {
             incoming.Id = _nextID;
-            _nextID ++;
+            _nextID++;
 
             Students.Add(incoming);
 
             return CreatedAtAction(
                 actionName: nameof(GetByID),
-                routeValues: new {id = incoming.Id},
+                routeValues: new { id = incoming.Id },
                 value: incoming
             );
         }
@@ -55,31 +73,32 @@ namespace Student_Registry.Controllers
         public ActionResult<bool> Update(int id, [FromBody] StudentRegistry incoming)
         {
             StudentRegistry? student = Students.FirstOrDefault(c => c.Id == id);
-                
-                if (student == null)
-                {
-                    return NotFound($"No student exists with id {id}.");
-                }
 
-                student.Firstname = incoming.Firstname;
-                student.Lastname = incoming.Lastname;
-                student.Hobby = incoming.Hobby;
-                student.Email = incoming.Email;
-                student.Slackname = incoming.Slackname;
+            if (student == null)
+            {
+                return NotFound($"No student exists with id {id}.");
+            }
 
-                return Ok(true);
-            } 
-        
+            student.Firstname = incoming.Firstname;
+            student.Lastname = incoming.Lastname;
+            student.Hobby = incoming.Hobby;
+            student.Email = incoming.Email;
+            student.Slackname = incoming.Slackname;
+
+            return Ok(true);
+        }
+
 
         [HttpDelete("Delete/{id}")]
-        public ActionResult<bool> Delete (int id)
+        public ActionResult<bool> Delete(int id)
         {
-            StudentRegistry? student =Students.FirstOrDefault(c => c.Id == id);
+            StudentRegistry? student = Students.FirstOrDefault(c => c.Id == id);
 
-            if (student == null){
+            if (student == null)
+            {
 
-                return NotFound ($"No crew member with id {id}");
-                
+                return NotFound($"No crew member with id {id}");
+
             }
 
             Students.Remove(student);
